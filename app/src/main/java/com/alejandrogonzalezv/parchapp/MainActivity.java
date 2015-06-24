2da009ee0799dd2ff8e62bd10dcd626d07ed5095
Alejandro <alegv92@gmail.com>
2015-06-24 12:00:00 -0500

    login fb

--- app/src/main/java/com/alejandrogonzalezv/parchapp/MainActivity.java
+++ app/src/main/java/com/alejandrogonzalezv/parchapp/MainActivity.java
@@ -1,9 +1,29 @@
 package com.alejandrogonzalezv.parchapp;
 
+
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +32,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -35,5 +60,97 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+        private CallbackManager callbackManager;
+        private TextView textView;
+
+        private AccessTokenTracker accessTokenTracker;
+        private ProfileTracker profileTracker;
+        private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                displayMessage(profile);
+            }
+
+            @Override
+            public void onCancel() {  }
+
+            @Override
+            public void onError(FacebookException e) {   }
+        };
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+            callbackManager = CallbackManager.Factory.create();
+
+            accessTokenTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+                }
+            };
+            profileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                    displayMessage(newProfile);
+                }
+            };
+
+            accessTokenTracker.startTracking();
+            profileTracker.startTracking();
+
+            return rootView;
+        }
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState){
+            super.onViewCreated(view, savedInstanceState);
+            LoginButton loginButton = (LoginButton) 								view.findViewById(R.id.login_button);
+            textView = (TextView) view.findViewById(R.id.dataStatus);
+
+            loginButton.setReadPermissions("user_friends");
+            loginButton.setFragment(this);
+            loginButton.registerCallback(callbackManager, callback);
+        }
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+        @Override
+        public void onStop() {
+            super.onStop();
+            accessTokenTracker.stopTracking();
+            profileTracker.stopTracking();
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            Profile profile = Profile.getCurrentProfile();
+            displayMessage(profile);
+        }
+
+
+
+
+        private void displayMessage(Profile profile){
+            if(profile != null){
+                textView.setText(profile.getName());
+            }
+        }
+
     }
 }
--- app/src/main/java/com/alejandrogonzalezv/parchapp/splash.java
+++ app/src/main/java/com/alejandrogonzalezv/parchapp/splash.java
@@ -1,5 +1,6 @@
 package com.alejandrogonzalezv.parchapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,7 +12,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class splash extends ActionBarActivity {
+public class splash extends Activity {
     private static final long SPLASH_DELAY = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
