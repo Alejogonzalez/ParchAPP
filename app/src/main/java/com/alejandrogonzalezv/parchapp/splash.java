9ba562d437fc4f495d141a8c9e6183071875d2aa
Alejandro <alegv92@gmail.com>
2015-06-24 10:58:51 -0500

    Comentario Inicial

---
+++ app/src/main/java/com/alejandrogonzalezv/parchapp/splash.java
@@ -0,0 +1,36 @@
+package com.alejandrogonzalezv.parchapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class splash extends ActionBarActivity {
+    private static final long SPLASH_DELAY = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(splash.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task,SPLASH_DELAY);
+
+    }
+
+
+
+}
